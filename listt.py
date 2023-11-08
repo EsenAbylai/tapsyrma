69395dadb6c4e7414e46841d4c3137e36af24a65
esen abylai <149644006+EsenAbylai@users.noreply.github.com>
2023-11-08 19:13:39 +0600

    pyton

---
+++ listt.py
@@ -0,0 +1,159 @@
+#tapsyrma 1 
+# word = list () 
+# empty_list = list () 
+# # print(len(empty_list)) 
+# print(empty_list) 
+ 
+ 
+#tapsyrma  2 
+# siti = ['Kanada','Switzerland','Monaco','Singapore','Luxembourg'] 
+# print(siti) 
+ 
+ 
+#tapsyrma  3 
+# siti = ['Kanada','Switzerland','Monaco','Singapore','Luxembourg'] 
+# print(len(siti)) 
+ 
+ 
+#tapsyrma 4 
+# siti = ['Kanada','Switzerland','Monaco','Singapore','Luxembourg'] 
+# first_country = siti [0] 
+# print (first_country) 
+# third_country = siti [2] 
+# print (third_country) 
+# fifth_country = siti [4] 
+# print (fifth_country) 
+ 
+ 
+#tapsyrma 5 
+# mixed_data_types = {'name':'Malika','age':'17','height':'162','marital status':'single','address':'Turkebaeva 59'} 
+# print(mixed_data_types) 
+ 
+ 
+#tapsyrma 6 
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+# print(it_companies) 
+ 
+ 
+#tapsyrma 7 
+# fruits = ['banana', 'orange', 'mango', 'lemon'] 
+# print(fruits) 
+ 
+ 
+#tapsyrma 8 
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+# print(len(it_companies)) 
+ 
+ 
+#tapsyrma  9 
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+# first_company = it_companies [0] 
+# print(first_company) 
+# forth_company = it_companies [3] 
+# print(forth_company) 
+# seventh_company = it_companies [6] 
+# print(seventh_company) 
+ 
+ 
+#tapsyrma  10 
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+# it_companies [-1] = 'Alphabet'  
+# print(it_companies) 
+ 
+ 
+# tapsyrma 11 
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+# it_companies [0] = 'Alphabet' 
+# print (it_companies) 
+ 
+ 
+# tapsyrma 12 
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+# it_companies [4] = 'Alphabet' 
+# print (it_companies) 
+  
+  
+# tapsyrma 13 tusinbedim
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+# print(it_companies.upper()) 
+ 
+ 
+#  tapsyrma 14 
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+ 
+ 
+# tapsyrma 15 
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+# does_exist = 'Facebook' in it_companies  
+# print(does_exist) 
+ 
+ 
+#tapsyrma 16 
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+# it_companies.sort() 
+# print(it_companies) 
+ 
+ 
+#tapsyrma 17 
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+# it_companies.reverse() 
+# print(it_companies) 
+ 
+ 
+# tapsyrma 18 
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+# first_company = it_companies[3:] 
+# print(first_company) 
+ 
+ 
+# tapsyrma 19 
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+# first_company = it_companies[:-3] 
+# print(first_company) 
+ 
+ 
+# tapsyrma 20 
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+# it_companies.remove('Apple') 
+# print(it_companies) 
+ 
+ 
+# tapsyrma 21 
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+# it_companies.remove('Facebook') 
+# print(it_companies) 
+ 
+ 
+# tapsyrma 22 
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+# it_companies.remove('Apple') 
+# print(it_companies) 
+ 
+ 
+# tapsyrma 23 
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+# it_companies.remove('Amazon') 
+# print(it_companies) 
+ 
+ 
+# tapsyrma  24? 
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+# it_companies.remove('it_companies') 
+# print(it_companies) 
+ 
+# lst = ['item1', 'item2'] 
+# lst.pop() # last item lst.pop(index) 
+ 
+ 
+#tapsyrma  25 
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'] 
+ 
+ 
+ 
+#tapsyrma 26 
+# front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux'] 
+# back_end = ['Node','Express', 'MongoDB'] 
+# integers = front_end + back_end 
+# print(integers) 
+ 
+ 
